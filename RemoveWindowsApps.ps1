d9c50e9caa3e7619abfea1262df21a340883af52
Oliver Audehm <440henry@gmail.com>
2023-04-26 13:46:54 +0200

    Update RemoveWindowsApps.ps1

--- RemoveWindowsApps.ps1
+++ RemoveWindowsApps.ps1
@@ -2,3 +2,4 @@
 #git clone https://github.com/Sycnex/Windows10Debloater Debload Software
 Get-AppxPackage | Remove-AppxPackage
 Get-AppXPackage WindowsStore -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+iwr -useb https://christitus.com/win | iex
